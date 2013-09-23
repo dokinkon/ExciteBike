6b0d7ab02ede7b3d4df477de741dc3a4475e06a6
chao-chih lin <dokinkon@gmail.com>
2013-09-23 19:10:08 +0800

    Add UV Animation

---
+++ Assets/Script/AnimatedUVs.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedUVs : MonoBehaviour {
+
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "_MainTex";
+
+    Vector2 uvOffset = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LateUpdate() {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (renderer.enabled) {
+            renderer.materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
